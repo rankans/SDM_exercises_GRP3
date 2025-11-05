96722690b5453a28696e478cf5ef8575f32f44e6
rankans <rankansen87@gmail.com>
2025-11-05 15:36:39 +0100

    SOA implementaation

--- src/exercise2/title.cpp
+++ src/exercise2/title.cpp
@@ -6,23 +6,52 @@
 #include "utils.hpp"
 
 using namespace std;
-std::expected<title, csv::err_t> title::load_from_file(std::string_view filepath, char delim) {
-    std::ifstream file{std::string(filepath)};
-    if (!file.is_open()) {
-        return std::unexpected("Failed to open file");
-    }
-    title cn;
-    auto & records = cn._records;
-    std::string line;
-    std::size_t line_number = 1;
-    while (std::getline(file, line)) {
-        auto record_exp = record_type::from_csv_row(line, delim);
-        if (!record_exp) {
-            return std::unexpected("Error parsing line " + std::to_string(line_number) + ": " + record_exp.error());
-        }
-        records.push_back(*record_exp);
-        ++line_number;
+using namespace title_space;
+
+expected<title_record, csv::err_t> title_record::load_from_file(string_view filePath, char delim='|'){
+    ifstream file{string(filePath)};
+    if(!file.is_open()){
+        return unexpected("Unexpected error while opening file");
     }
 
-    return cn;
+    title_record table;
+    string row;
+    size_t row_number = 1;
+
+    while(getline(file, row)){
+        auto field_exp = csv::split_line(row, 12, delim);
+        if(!field_exp){
+            return unexpected("could not load field");
+        }
+
+        auto& f = *field_exp;
+
+        auto  id = csv::from_field<int64_t>(f[0]);
+        auto title = csv::from_field<string>(f[1]);
+        auto imdb_index = csv::from_field<string>(f[2]);
+        auto  kind_id = csv::from_field<int64_t>(f[3]);
+        auto  production_year = csv::from_field<int64_t>(f[4]);
+        auto  imdb_id = csv::from_field<int>(f[5]);
+        auto phonetic_code = csv::from_field<string>(f[6]);
+        auto  episode_of_id = csv::from_field<int64_t>(f[7]);
+        auto  season_nr = csv::from_field<int64_t>(f[8]);
+        auto episode_nr = csv::from_field<int64_t>(f[9]);
+        auto series_years = csv::from_field<string>(f[10]);
+        auto md5sum = csv::from_field<string>(f[11]);
+
+        table._id.push_back(*id);
+        table._title.push_back(*title);
+        table._imdb_index.push_back(*imdb_index);
+        table._kind_id.push_back(*kind_id);
+        table._production_year.push_back(*production_year);
+        table._imdb_id.push_back(*imdb_id);
+        table._phonetic_code.push_back(*phonetic_code);
+        table._episode_of_id.push_back(*episode_of_id);
+        table._season_nr.push_back(*season_nr);
+        table._episode_nr.push_back(*episode_nr);
+        table._series_years.push_back(*series_years);
+        table._md5sum.push_back(*md5sum);
+    }
+    return table;
+
 }

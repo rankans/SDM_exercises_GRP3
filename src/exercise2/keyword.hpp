48f46eba453d3d4ad8c1534a3f6b37909180371f
Puneeth Y <pyd773@gmail.com>
2025-12-01 04:31:08 +0100

    Refactor keyword.hpp: improve formatting and reorganize class structure

--- src/exercise2/keyword.hpp
+++ src/exercise2/keyword.hpp
@@ -9,18 +9,20 @@
 
 // imdb=# \d keyword
 //                         Table "public.keyword"
-//     Column     |         Type         | Collation | Nullable | Default 
+//     Column     |         Type         | Collation | Nullable | Default
 // ---------------+----------------------+-----------+----------+---------
-//  id            | integer              |           | not null | 
-//  keyword       | text                 |           | not null | 
-//  phonetic_code | character varying(5) |           |          | 
+//  id            | integer              |           | not null |
+//  keyword       | text                 |           | not null |
+//  phonetic_code | character varying(5) |           |          |
 // Indexes:
 //     "keyword_pkey" PRIMARY KEY, btree (id)
 
 using namespace std;
 
-namespace keyword_space{
-    class keyword_record{
+namespace keyword_space
+{
+    class keyword_record
+    {
 
         private:
             vector<int64_t> _id;
@@ -36,10 +38,14 @@ namespace keyword_space{
                     };
             static expected<keyword_record, csv::err_t> load_from_file(string_view filePath, char delim='|');
 
-            //getters
-            const vector<int64_t>& id()                   const noexcept {return _id;}         
-            const vector<string>& keyword()  const noexcept {return _keyword;}
-            const vector<string>& phonetic_code()  const noexcept {return _phonetic_code;}
+    public:
+        struct materialized_result
+        {
+            std::vector<int64_t> id;
+            std::vector<std::string> keyword;
+            std::vector<std::string> phonetic_code;
+        };
+        static expected<keyword_record, csv::err_t> load_from_file(string_view filePath, char delim = '|');
 
             // For lab 5 batch operations
             batch next_batch(size_t& pointer) const{
@@ -54,15 +60,18 @@ namespace keyword_space{
             //setter
             void set_keyword(size_t i, const string& modified_value){_keyword[i] = modified_value;}
 
-            size_t size() const {return _id.size();}
+        // setter
+        void set_keyword(size_t i, const string &modified_value) { _keyword[i] = modified_value; }
 
-            void print_record(size_t i) const{
-                cout<< "Keyword_record { "
-                << "id: " << _id[i] << ", "
-                << "keyword: " << _keyword[i] << ", "
-                << "phonetic_code: " << _phonetic_code[i] << "}\n ";
-            }
+        size_t size() const { return _id.size(); }
 
+        void print_record(size_t i) const
+        {
+            cout << "Keyword_record { "
+                 << "id: " << _id[i] << ", "
+                 << "keyword: " << _keyword[i] << ", "
+                 << "phonetic_code: " << _phonetic_code[i] << "}\n ";
+        }
     };
 
 }

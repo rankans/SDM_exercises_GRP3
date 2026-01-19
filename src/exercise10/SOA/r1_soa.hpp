0d53d0227a4d7afaa9b7e4828cc0cacd8f21c18c
rankans <rankansen87@gmail.com>
2026-01-19 10:35:13 +0100

    Fixed exercise 10

---
+++ src/exercise10/SOA/r1_soa.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class r1_soa
+{
+private:
+    std::vector<std::int64_t> _column_a;
+    std::vector<std::string> _column_b;
+
+    r1_soa() = default;
+
+public:
+    static r1_soa generate_relation(std::size_t X, std::int64_t Y);
+
+    const std::vector<std::int64_t> &column_a() const noexcept { return _column_a; }
+    const std::vector<std::string> &column_b() const noexcept { return _column_b; }
+
+    std::size_t size() const noexcept { return _column_a.size(); }
+};
